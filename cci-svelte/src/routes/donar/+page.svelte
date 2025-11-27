a480c7213483ffc4e94a580e1c94267bd46c5ad1
Valdivia94x <104047145+Valdivia94x@users.noreply.github.com>
2025-11-27 15:34:35 -0600

    content: update donar page information

--- cci-svelte/src/routes/donar/+page.svelte
+++ cci-svelte/src/routes/donar/+page.svelte
@@ -6,15 +6,15 @@
 	// Datos de donación (ejemplo - puedes actualizarlos después)
 	const donationData = {
 		bankInfo: {
-			bank: 'Banco Ejemplo',
+			bank: 'Banamex',
 			accountName: 'Consejo Cívico de las Instituciones, A.C.',
-			accountNumber: '1234 5678 9012 3456',
-			clabe: '012345678901234567',
+			accountNumber: '70036183924',
+			clabe: '002060700361839245',
 			swift: 'BANCMXMM',
-			concept: 'Donativo CCI Laguna'
+			concept: 'Aportación CCI Laguna'
 		},
 		invoicing: {
-			email: 'facturacion@ccilaguna.org.mx',
+			email: 'contacto@ccilaguna.org.mx',
 			rfc: 'CCI-120210-LK7'
 		},
 		deductible: true
@@ -49,9 +49,8 @@
 		<div class="hero-content">
 			<h1>Apoya Nuestra Causa</h1>
 			<p class="hero-description">
-				Tu donativo nos ayuda a continuar promoviendo la participación ciudadana y desarrollar
-				mejores gobiernos en la Comarca Lagunera. Cada aportación cuenta y es deducible de
-				impuestos.
+				Con tu ayuda podemos construir un futuro más justo y más seguro, con ciudadanos informados y
+				comprometidos, a través de la participación ciudadana
 			</p>
 		</div>
 	</section>
@@ -94,30 +93,6 @@
 				</div>
 				<h3>CLABE Interbancaria</h3>
 				<p class="info-value">{donationData.bankInfo.clabe}</p>
-			</div>
-
-			<div class="info-card">
-				<div class="info-icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" fill="currentColor">
-						<path
-							d="M48 0C21.5 0 0 21.5 0 48V464c0 26.5 21.5 48 48 48h96V432c0-26.5 21.5-48 48-48s48 21.5 48 48v80h96c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48H48zM64 240c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V240zm112-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V240zM80 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16zm80 16c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16V112zM272 96h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H272c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16z"
-						/>
-					</svg>
-				</div>
-				<h3>Beneficiario</h3>
-				<p class="info-value">{donationData.bankInfo.accountName}</p>
-			</div>
-
-			<div class="info-card">
-				<div class="info-icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor">
-						<path
-							d="M0 128C0 92.7 28.7 64 64 64H256h48 16H576c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H320 304 256 64c-35.3 0-64-28.7-64-64V128zm320 0V384H576V128H320zM178.3 175.9c-3.2-7.2-10.4-11.9-18.3-11.9s-15.1 4.7-18.3 11.9l-64 144c-4.5 10.1 .1 21.9 10.2 26.4s21.9-.1 26.4-10.2l8.9-20.1h73.6l8.9 20.1c4.5 10.1 16.3 14.6 26.4 10.2s14.6-16.3 10.2-26.4l-64-144zM160 233.2L179 276H141l19-42.8zM448 164c11 0 20 9 20 20v4h44 16c11 0 20 9 20 20s-9 20-20 20h-2l-1.6 4.5c-8.9 24.4-22.4 46.6-39.6 65.4c.9 .6 1.8 1.1 2.7 1.6l18.9 11.3c9.5 5.7 12.5 18 6.9 27.4s-18 12.5-27.4 6.9l-18.9-11.3c-4.5-2.7-8.8-5.5-13.1-8.5c-10.6 7.5-21.9 14-34 19.4l-3.6 1.6c-10.1 4.5-21.9-.1-26.4-10.2s.1-21.9 10.2-26.4l3.6-1.6c6.4-2.9 12.6-6.1 18.5-9.8l-12.2-12.2c-7.8-7.8-7.8-20.5 0-28.3s20.5-7.8 28.3 0l14.6 14.6 .5 .5c12.4-13.1 22.5-28.3 29.8-45H448 376c-11 0-20-9-20-20s9-20 20-20h52v-4c0-11 9-20 20-20z"
-						/>
-					</svg>
-				</div>
-				<h3>SWIFT (Transferencias Internacionales)</h3>
-				<p class="info-value">{donationData.bankInfo.swift}</p>
 			</div>
 
 			<div class="info-card concept-card">
